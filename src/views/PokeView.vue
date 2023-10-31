f40381e899e1702132e22d8e36a675552475fab5
MateoChacon <xdmateo90@gmail.com>
2023-10-31 10:35:50 -0500

    78. useRoute (params)

--- src/views/PokeView.vue
+++ src/views/PokeView.vue
@@ -1,3 +1,22 @@
+<script setup>
+import axios from "axios"
+import { useRoute, useRouter } from "vue-router";
+
+const route = useRoute();
+const router = useRouter();
+
+const getData = async () => {
+    try {
+        const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon/${route.params.name}`)
+        console.log(data)
+    } catch (error) {
+        console.error(error)
+    }
+}
+getData()
+
+</script>
+
 <template>
     <h1>Poke: {{ $route.params.name }}</h1>
 </template>
