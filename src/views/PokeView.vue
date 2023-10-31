c660ef85d601b4a202d6c3abbb7ef0f98c9604ff
MateoChacon <xdmateo90@gmail.com>
2023-10-31 10:51:15 -0500

    80. Pintar información del Pokemon

--- src/views/PokeView.vue
+++ src/views/PokeView.vue
@@ -1,16 +1,23 @@
 <script setup>
 import axios from "axios"
+import { ref } from 'vue'
 import { useRoute, useRouter } from "vue-router";
 
 const route = useRoute();
 const router = useRouter();
+const pokeSprite = ref({});
+
+const back = () => {
+    router.push("/pokemon");
+};
 
 const getData = async () => {
     try {
         const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon/${route.params.name}`)
-        console.log(data)
+        pokeSprite.value = data.sprites.front_default;
     } catch (error) {
         console.error(error)
+        pokeSprite.value = null;
     }
 }
 getData()
@@ -18,5 +25,10 @@ getData()
 </script>
 
 <template>
-    <h1>Poke: {{ $route.params.name }}</h1>
+    <div v-if="pokeSprite">
+        <img :src="pokeSprite" alt="" />
+        <h1>Poke: {{ $route.params.name }}</h1>
+    </div>
+    <h1 v-else>Pokemon no encontrado...</h1>
+    <button @click="back()">Volver al listado</button>
 </template>
