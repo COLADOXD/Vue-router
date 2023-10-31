cb2e6b78d889b7688be8017de0b31e69a1a33d3e
MateoChacon <xdmateo90@gmail.com>
2023-10-31 09:35:30 -0500

    73. Axios

--- src/views/PokemonView.vue
+++ src/views/PokemonView.vue
@@ -1,4 +1,19 @@
-<script setup></script>
+<script setup>
+import axios from "axios"
+import { ref } from "vue";
+
+const pokemons = ref([])
+
+const getData = async () => {
+    try {
+        const { data } = await axios.get("https://pokeapi.co/api/v2/pokemon");
+        console.log(data.results)
+    } catch (e) {
+        console.log(e)
+    }
+}
+getData()
+</script>
 
 <template>
     <h1>
