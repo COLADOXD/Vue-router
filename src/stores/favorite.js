8ddb24c8a786915a4aba24eb3fb5abb3fe9b46df
MateoChacon <xdmateo90@gmail.com>
2023-11-01 13:18:52 -0500

    101. LocalStorage

--- src/stores/favorite.js
+++ src/stores/favorite.js
@@ -4,13 +4,18 @@ import { ref } from "vue";
 export const useFavoriteStore = defineStore("favorite", () => {
   const favorites = ref([]);
 
+  if (localStorage.getItem("favorites")) {
+    favorites.value = JSON.parse(localStorage.getItem("favorites"));
+  }
+
   const addFavorite = (poke) => {
     favorites.value.push(poke);
+    localStorage.setItem("favorites", JSON.stringify(favorites.value));
   };
 
   const remove = (id) => {
-    console.log(favorites.value.filter((item) => item.id !== id));
     favorites.value = favorites.value.filter((item) => item.id !== id);
+    localStorage.setItem("favorites", JSON.stringify(favorites.value));
   };
 
   const findPoke = (poke) => favorites.value.find((item) => item.name === poke);
